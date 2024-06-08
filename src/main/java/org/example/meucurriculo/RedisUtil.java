89f9a25bb9b95d20b09824f123d0b017d04de8d1
gabriel <tuladel.123.team2020@hotmail.com>
2024-06-08 00:49:47 -0300

    v1

--- src/main/java/org/example/meucurriculo/RedisUtil.java
+++ src/main/java/org/example/meucurriculo/RedisUtil.java
@@ -1,13 +1,24 @@
 package org.example.meucurriculo;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisClientConfig;
+import redis.clients.jedis.JedisPoolConfig;
+
+import redis.clients.jedis.Jedis;
 
 public class RedisUtil {
 
     public static Jedis getConnection() {
-        String redisHost = System.getenv("REDISHOST");
-        int redisPort = Integer.parseInt(System.getenv("REDISPORT"));
-        System.out.println(redisHost + ":" + redisPort);
-        return new Jedis(redisHost, redisPort);
+        String redisHost = System.getenv("REDIS_HOST");
+        int redisPort = Integer.parseInt(System.getenv("REDIS_PORT"));
+        String redisPassword = System.getenv("REDIS_PASSWORD");
+
+        Jedis jedis = new Jedis(redisHost, redisPort);
+
+        if (redisPassword != null && !redisPassword.isEmpty()) {
+            jedis.auth(redisPassword);
+        }
+
+        return jedis;
     }
 }
