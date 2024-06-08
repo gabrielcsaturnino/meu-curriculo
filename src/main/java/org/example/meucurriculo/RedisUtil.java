abefdb4ed31d3c235b81a0f8037fc9180043128b
gabriel <tuladel.123.team2020@hotmail.com>
2024-06-08 00:08:12 -0300

    v1

--- src/main/java/org/example/meucurriculo/RedisUtil.java
+++ src/main/java/org/example/meucurriculo/RedisUtil.java
@@ -3,11 +3,10 @@ package org.example.meucurriculo;
 import redis.clients.jedis.Jedis;
 
 public class RedisUtil {
-    private static final String REDIS_HOST = "redis";
-    private static final int REDIS_PORT = 6379;
-
 
     public static Jedis getConnection() {
-        return new Jedis(REDIS_HOST, REDIS_PORT);
+        String redisHost = System.getenv("REDIS_HOST");
+        int redisPort = Integer.parseInt(System.getenv("REDIS_PORT"));
+        return new Jedis(redisHost, redisPort);
     }
 }
