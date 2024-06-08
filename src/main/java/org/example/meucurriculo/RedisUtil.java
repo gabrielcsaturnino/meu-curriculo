58c04fb24dedf6a22dd7186f226156ba79400598
gabriel <tuladel.123.team2020@hotmail.com>
2024-06-08 00:29:58 -0300

    v1

--- src/main/java/org/example/meucurriculo/RedisUtil.java
+++ src/main/java/org/example/meucurriculo/RedisUtil.java
@@ -7,6 +7,7 @@ public class RedisUtil {
     public static Jedis getConnection() {
         String redisHost = System.getenv("REDISHOST");
         int redisPort = Integer.parseInt(System.getenv("REDISPORT"));
+        System.out.println(redisHost + ":" + redisPort);
         return new Jedis(redisHost, redisPort);
     }
 }
