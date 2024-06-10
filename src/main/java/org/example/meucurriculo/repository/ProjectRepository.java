4c841b4f4de67319a01ff0b368239dc1b17dfcf2
gabriel <tuladel.123.team2020@hotmail.com>
2024-06-10 12:27:46 -0300

    v2

--- src/main/java/org/example/meucurriculo/repository/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.meucurriculo.repository;
-
-import org.example.meucurriculo.models.Project;
-
-public interface ProjectRepository {
-     void saveProject(Project project);
-     Project getProject(String id);
-}
--- src/main/java/org/example/meucurriculo/services/ProjectService.java
+++ src/main/java/org/example/meucurriculo/services/ProjectService.java
@@ -2,18 +2,9 @@ package org.example.meucurriculo.services;
 
 
 import org.example.meucurriculo.models.Project;
-import org.example.meucurriculo.repositoryimpl.ProjectRepositoryImpl;
 
 
 public class ProjectService {
-    ProjectRepositoryImpl projectRepositoryImpl;
-
-
-    public void saveProject(Project project){
-        projectRepositoryImpl = new ProjectRepositoryImpl();
-        projectRepositoryImpl.saveProject(checkNameLength(project));
-
-    }
 
     public Project checkNameLength(Project project) {
         if(project.getNome().length()<=254){
@@ -24,12 +15,5 @@ public class ProjectService {
         }
     }
 
-    public Project getProject(String id) {
-        if (projectRepositoryImpl.getProject(id) != null) {
-            return projectRepositoryImpl.getProject(id);
-        }else {
-            saveProject(new Project("1", "nome", "descricao"));
-            return projectRepositoryImpl.getProject(id);
-        }
-    }
+
 }
